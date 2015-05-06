211ed5a0c35791cb456d450029ebf46881bcc115
Sam Hendrickx <samhendrickx@gmail.com>
2015-05-06 15:32:34 +0200

    Handler added'

---
+++ src/PHPErrorHandler/ErrorHandler.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace PHPErrorHandler;
+
+use PHPErrorHandler\GitHub\ErrorBody;
+use PHPErrorHandler\GitHub\GitHubService;
+
+class ErrorHandler {
+	private $mail = true;
+	private $emailAddress = "";
+
+	private $github = true;
+	private $githubUsername = "";
+	private $githubPassword = "";
+	private $githubRepository = "";
+
+	public function __construct() {
+
+	}
+	public function handleErrors() {
+		//register_shutdown_function('PHPErrorHandler::errorHandler');
+		set_error_handler(array($this, 'errorHandler'), E_ALL);
+	}
+
+	public function errorHandler($errno = '', $errstr = '', $errfile = '', $errline = -1) {
+		if (!(error_reporting() & $errno)) {
+	        // This error code is not included in error_reporting
+	        return;
+	    }
+
+	    $title = $errstr . ' (' . end(explode('/',$errfile)) . ')';
+	    $body = new ErrorBody($errno, $errstr, $errfile, $errline);
+
+	    // Call API
+	    $githubService = new GitHubService($this->githubUsername, $this->githubPassword, $this->githubRepository);
+	    
+	    $githubService->createIssue($title, $body);
+
+	    /* Don't execute PHP internal error handler */
+	    return true;
+	}
+	
+}
